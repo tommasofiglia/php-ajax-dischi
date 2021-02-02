d04b2c927fdfd0358b681d3593da152441928aa6
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-02 15:27:17 +0100

    Commit iniziale - SASS inizializzato

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dischi con php</title>
+    <link rel="stylesheet" href="dist/css/app.css">
+  </head>
+  <body>
+    <h1>prova</h1>
+
+
+
+
+    <!-- Script vueJS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+    <!-- Script Javascript -->
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    
+  </body>
+</html>
